6814fe5a9c5595c2eabee01bc4074db0893e17d0
Matthew Chan <hohin.chan@sjsu.edu>
2021-10-08 21:01:48 -0700

    Added calculator function to calculator.py.

--- calculator.py
+++ calculator.py
@@ -6,4 +6,29 @@ def parse_input():
     inputList = inputString.split(" ")
     calculator(inputList[0], inputList[2], inputList[1]) #order is 0, 2, 1 since the middle is the operator, which corresponds with index 1
 
-parse_input()
+def calculator(number1, number2, operator):
+    """
+    Calculates the correct value from the equation that was taken from the user.
+
+    If the operator or numbers are invalid, the function exits and returns the boolean value false.
+    """
+    try: #use a try and except block to test whether or not we can convert the 2 numbers into floats for valid input
+        number1 = float(number1)
+        number2 = float(number2)
+    except ValueError:
+        return False
+
+    if operator == "+":
+        return number1 + number2
+    elif operator == "-":
+        return number1 - number2
+    elif operator == "*":
+        return number1 * number2
+    elif operator == "/":
+        return number1 / number2
+    elif operator == "//":
+        return number1 // number2
+    elif operator == "**":
+        return number1 ** number2
+    else:
+        return False
