8b4cfe4760cefba5c6cc6649aab8df35ea6871b6
Matthew Chan <hohin.chan@sjsu.edu>
2021-10-11 10:56:42 -0700

    Clarified and added docstring to function in multiply.py.

--- multiply.py
+++ multiply.py
@@ -3,10 +3,17 @@ def multiply_list():
     Multiplies all of the events in a user inputted list and returns the value.
 
     If an element in the list is not a valid integer, this function will return the boolean value false.
+
+    Returns
+    -------
+    int
+        If the input is valid, returns the product of all the integers in the list.
+    boolean
+        False if the input is not valid.
     """
     multStr = input("Input: ")
     result = 1
-    multStr = multStr[1:len(multStr) - 1] # need to use slicing to exclude the brackets at either side of the user input
+    multStr = multStr[1:len(multStr) - 1] # need to use slicing to exclude the brackets at either side of the user input since right now the input is a string.
     multList = multStr.split(", ")
     for x in multList:
         # use isdigit() to check if the element in the list is actually an integer so we do not get an error
