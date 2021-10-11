7e682dc4c9673480b4b30b868bb1340b83bbed09
Matthew Chan <hohin.chan@sjsu.edu>
2021-10-11 11:49:26 -0700

    Created and tried implementing timing.py. Also made triple.py file for the last part of the homework.

---
+++ timing.py
@@ -0,0 +1,18 @@
+import time
+
+def my_decorator(func):
+    def wrapper():
+        x = func()
+        time.sleep(2)
+        y = func()
+        # help from https://realpython.com/python-f-strings/ for f strings
+        print(f"Total time: {y - x}")
+    return wrapper
+
+def calculate_time():
+    # help from https://realpython.com/primer-on-python-decorators/#simple-decorators for decorators
+    x = time.time()
+    return x
+
+calculate_time = my_decorator(calculate_time)
+calculate_time()
