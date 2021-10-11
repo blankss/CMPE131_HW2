d5c473a48c613cb456e9745f88dd87cbfbbd2484
Matthew Chan <hohin.chan@sjsu.edu>
2021-10-11 10:53:27 -0700

    Clarified and added descriptive docstring to the functions in calculator.py.

--- calculator.py
+++ calculator.py
@@ -1,16 +1,44 @@
 def parse_input():
     """
-    Parses the input and passes the parsed input correctly into the calculator.
+    Parses the input and passes the parsed input correctly into the calculator function.
+
+    Returns
+    -------
+    float
+
+    The calculated float from passing in the parameters of the operands and the operation symbol to calculator.
     """
     inputString = input("Enter equation: ")
     inputList = inputString.split(" ")
-    calculator(inputList[0], inputList[2], inputList[1]) #order is 0, 2, 1 since the middle is the operator, which corresponds with index 1
+    return calculator(inputList[0], inputList[2], inputList[1]) #order is 0, 2, 1 since the middle is the operator, which corresponds with index 1
 
 def calculator(number1, number2, operator):
     """
-    Calculates the correct value from the equation that was taken from the user.
+    Calculates the correct value from the equation that was taken from the user. 
 
-    If the operator or numbers are invalid, the function exits and returns the boolean value false.
+    If the operator or numbers are invalid, the function exits and returns the boolean value false. Valid operators are 
+    +, -, *, /, //, and ** which are addition, subtraction, multiplication, division, integer division, and power respectively.
+
+    Parameters
+    ----------
+    number1: float
+        First operand.
+    number2: float
+        Second operand.
+    operator: string
+        Operator to do an operation of number1 operator number2.
+
+    Returns
+    -------
+    float
+        The operation of ``number1`` and ``number2``.
+    
+    Examples
+    --------
+    Enter equation: 10 + 11
+    21.0
+    Enter equation: 10 ** 2
+    100.0
     """
     try: #use a try and except block to test whether or not we can convert the 2 numbers into floats for valid input
         #https://www.tutorialspoint.com/python/python_exceptions.htm for try except handling help
