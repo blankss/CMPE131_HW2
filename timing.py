68bc9510efbca958ada4d1bce590e63a5f0dd312
Matthew Chan <hohin.chan@sjsu.edu>
2021-10-11 21:33:33 -0700

    Fixed timing.py to accept a function in the other function.

--- timing.py
+++ timing.py
@@ -1,18 +1,33 @@
 import time
 
-def my_decorator(func):
-    def wrapper(func):
-        x = func()
-        time.sleep(2)
-        y = func()
+def calculate_time(func):
+    """"
+    Calculates and prints out the time to execute func.
+
+    Parameters
+    ----------
+    func: function
+        The function to wrap the time wrapper around.
+
+    Returns
+    -------
+    function
+        The wrapped function.
+    """
+    # help for args/kwargs https://www.geeksforgeeks.org/args-kwargs-python/
+    def wrapper(*args):
+        """"
+        Wraps the func with the wrapper to calculate the runtime and print it out.
+
+        Parameters
+        ----------
+        *args: any value
+            The value to pass into the function to time.
+        """
+        # help with decorators from https://realpython.com/primer-on-python-decorators/
+        start = time.time()
+        func(*args) #still need to execute the function so we sandwich it between start and end to time it
+        end = time.time()
         # help from https://realpython.com/python-f-strings/ for f strings
-        print(f"Total time: {y - x}")
+        print(f"Total time: {end - start}")
     return wrapper
-
-def calculate_time():
-    # help from https://realpython.com/primer-on-python-decorators/#simple-decorators for decorators
-    x = time.time()
-    return x
-
-calculate_time = my_decorator(calculate_time)
-calculate_time()
