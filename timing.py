b194bf78e3b9bd07c23aa912e4ea62b217e9b707
Matthew Chan <hohin.chan@sjsu.edu>
2021-10-12 14:39:20 -0700

    Fixed indentation error for docstring.

--- timing.py
+++ timing.py
@@ -1,8 +1,7 @@
 import time
 
 def calculate_time(func):
-    def wrapper():
-    """"
+     """"
     Wraps the func with the wrapper to calculate the runtime and print it out.
 
     Parameters
@@ -10,6 +9,7 @@ def calculate_time(func):
     func: the function to wrap timer around.
         The value to pass into the function to time.
     """
+    def wrapper():
     # help with decorators from https://realpython.com/primer-on-python-decorators/
     start = time.time()
     func(*args) #still need to execute the function so we sandwich it between start and end to time it
