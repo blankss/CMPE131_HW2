131c65e0486da803bca4319d6834a0499840576b
Matthew Chan <hohin.chan@sjsu.edu>
2021-10-11 21:35:56 -0700

    Added func argument to wrapper.

--- timing.py
+++ timing.py
@@ -1,33 +1,18 @@
 import time
 
-def calculate_time(func):
+def wrapper(func):
     """"
-    Calculates and prints out the time to execute func.
+    Wraps the func with the wrapper to calculate the runtime and print it out.
 
     Parameters
     ----------
-    func: function
-        The function to wrap the time wrapper around.
-
-    Returns
-    -------
-    function
-        The wrapped function.
+    func: the function to wrap timer around.
+        The value to pass into the function to time.
     """
-    # help for args/kwargs https://www.geeksforgeeks.org/args-kwargs-python/
-    def wrapper(*args):
-        """"
-        Wraps the func with the wrapper to calculate the runtime and print it out.
-
-        Parameters
-        ----------
-        *args: any value
-            The value to pass into the function to time.
-        """
-        # help with decorators from https://realpython.com/primer-on-python-decorators/
-        start = time.time()
-        func(*args) #still need to execute the function so we sandwich it between start and end to time it
-        end = time.time()
-        # help from https://realpython.com/python-f-strings/ for f strings
-        print(f"Total time: {end - start}")
-    return wrapper
+    # help with decorators from https://realpython.com/primer-on-python-decorators/
+    start = time.time()
+    func() #still need to execute the function so we sandwich it between start and end to time it
+    end = time.time()
+    # help from https://realpython.com/python-f-strings/ for f strings
+    print(f"Total time: {end - start}")
+return wrapper
